3cc7af3bcb24ff2512ac3fbdf3fd94ed054b1bef
James Charnley <85146744+JamesCharnley@users.noreply.github.com>
2026-02-26 18:15:22 +1300

    Move transition logic into state machine layers

---
+++ Assets/Scripts/NPC/StateMachine/SubMachines/SubStateMachine.cs
@@ -0,0 +1,38 @@
+using AICharacterModule.NPC.StateMachine.Core;
+using AICharacterModule.NPC.StateMachine.Managers;
+
+namespace AICharacterModule.NPC.StateMachine.SubMachines
+{
+    public class SubStateMachine<TLocalData, TGlobalData> : ISubStateMachine<TGlobalData>
+    {
+        private readonly string _entryState;
+
+        public SubStateMachine(string name, string entryState, StateManager<TLocalData, TGlobalData> stateManager)
+        {
+            Name = name;
+            _entryState = entryState;
+            StateManager = stateManager;
+        }
+
+        public string Name { get; }
+
+        public StateManager<TLocalData, TGlobalData> StateManager { get; }
+
+        public TGlobalData GlobalData => StateManager.GlobalData;
+
+        public void Enter()
+        {
+            StateManager.SwitchTo(_entryState);
+        }
+
+        public void Tick(float deltaTime)
+        {
+            StateManager.Tick(deltaTime);
+        }
+
+        public void Exit()
+        {
+            StateManager.ExitCurrent();
+        }
+    }
+}
