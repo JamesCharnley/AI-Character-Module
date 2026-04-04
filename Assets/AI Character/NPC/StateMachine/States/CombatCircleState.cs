05597fe85f0f16039b0c73641e625c5acda7ce09
JamesCharnley <james.anthony.charnley@gmail.com>
2026-04-04 21:48:52 +1300

    Add turning animation and logic

--- Assets/AI Character/NPC/StateMachine/States/CombatCircleState.cs
+++ Assets/AI Character/NPC/StateMachine/States/CombatCircleState.cs
@@ -6,7 +6,7 @@ namespace AICharacterModule.NPC.StateMachine.States
 {
     public class CombatCircleState : IState<CombatData, NPCGlobalData>
     {
-        private const float IdleDurationSeconds = 4f;
+        private const float IdleDurationSeconds = 8f;
         private const float OrbitMinDistanceFromNpc = 8f;
         private const float OrbitMaxDistanceFromNpc = 15f;
         private const float OrbitStopDistanceThreshold = 3.75f;
@@ -69,7 +69,20 @@ namespace AICharacterModule.NPC.StateMachine.States
 
         private void WhileIdle(CombatData localData, NPCGlobalData globalData, float deltaTime)
         {
+            if (globalData.NavAgent.speed == 0)
+            {
+                if (globalData.NavAgent.hasPath)
+                {
+                    globalData.NavAgent.ResetPath();
+                    globalData.NavAgent.isStopped = true;
+                }
+            }
             _idleTimer -= deltaTime;
+
+            if (!isFacingPlayer)
+            {
+                TurnToTarget(globalData);
+            }
 
             if (_idleTimer > 0f)
             {
@@ -92,6 +105,7 @@ namespace AICharacterModule.NPC.StateMachine.States
                 _idleTimer = IdleDurationSeconds;
                 IsIdle = true;
                 IsOrbiting = false;
+                isFacingPlayer = false;
                 return;
             }
 
@@ -100,6 +114,52 @@ namespace AICharacterModule.NPC.StateMachine.States
             _idleTimer = IdleDurationSeconds;
             IsIdle = false;
             IsOrbiting = true;
+            isFacingPlayer = false;
+            globalData.NavAgent.isStopped = false;
+        }
+
+        private bool isFacingPlayer = false;
+        void TurnToTarget(NPCGlobalData _globalData)
+        {
+            if (!isFacingPlayer)
+            {
+                Vector3 toPlayer = (_globalData.CurrentTarget.position - Vector3.up) -
+                                   _globalData.NpcTransform.position;
+
+// Ignore vertical difference if needed
+                toPlayer.y = 0;
+
+                float dot = Vector3.Dot(_globalData.NpcTransform.right, toPlayer);
+
+                if (dot > 0.1f)
+                {
+                    // Player is to the RIGHT of NPC
+                    _globalData.Anim.SetBool("IsTurningRight", true);
+                }
+                else if (dot < -0.1f)
+                {
+                    // Player is to the LEFT of NPC
+                    _globalData.Anim.SetBool("IsTurningLeft", true);
+                }
+                else
+                {
+                    _globalData.Anim.SetBool("IsTurningLeft", false);
+                    _globalData.Anim.SetBool("IsTurningRight", false);
+
+                    isFacingPlayer = true;
+                }
+
+                if (toPlayer.sqrMagnitude > 0.001f && !isFacingPlayer)
+                {
+                    Quaternion targetRotation = Quaternion.LookRotation(toPlayer);
+
+                    _globalData.NpcTransform.rotation = Quaternion.RotateTowards(
+                        _globalData.NpcTransform.rotation,
+                        targetRotation,
+                        120 * Time.deltaTime
+                    );
+                }
+            }
         }
 
         public void Exit(CombatData localData, NPCGlobalData globalData)
